e7e16120260d234f6cdd61a331a54308bb4ecb67
ye-seob <161628841+ye-seob@users.noreply.github.com>
2024-09-01 01:02:17 +0900

    Time: 0 ms (100%), Space: 41.7 MB (62.74%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,18 +1,16 @@
 class Solution {
-    public String mergeAlternately(String str1, String str2) {
+    public String mergeAlternately(String word1, String word2) {
+        int minLength = Math.min(word1.length(),word2.length());
         
-       int minLen = Math.min(str1.length(), str2.length());
+        StringBuilder sb = new StringBuilder();
 
-         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < minLen; i++) {
-            sb.append(str1.charAt(i));
-            sb.append(str2.charAt(i));
+        for(int i = 0; i < minLength; i++){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
         }
-        
-        sb.append(str1.substring(minLen)); //len부터 끝까지(substring메소드 인자가 하나라면,두 개면 첫인자부터 두번째 인자까지)
-        sb.append(str2.substring(minLen));
-        
+        sb.append(word1.substring(minLength));
+        sb.append(word2.substring(minLength));
+
         return sb.toString();
     }
 }
