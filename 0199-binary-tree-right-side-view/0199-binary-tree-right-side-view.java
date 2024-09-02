8ba9488a4081012cacc5b549bb731c33fc16e18f
ye-seob <161628841+ye-seob@users.noreply.github.com>
2024-09-02 20:53:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+   public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> answer = new ArrayList<>();
+    if (root == null) return answer;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+        int level = queue.size();
+        for (int i = 0; i < level; i++) {
+            TreeNode currentNode = queue.poll();
+            if (i == level - 1) {
+                result.add(currentNode.val);
+            }
+            if (currentNode.left != null) queue.add(currentNode.left);
+            if (currentNode.right != null) queue.add(currentNode.right);
+        }
+    }
+
+    return answer;
+}
+
+}
