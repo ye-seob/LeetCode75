0e1b44c98b50effdb72e39e3dc93f12c9f5aaf4e
ye-seob <161628841+ye-seob@users.noreply.github.com>
2024-09-11 10:03:03 +0900

    Time: 3 ms (99.75%), Space: 65.6 MB (12.03%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,26 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         ListNode current = head;
+//         if(head.next == null) return null;
+//         int length = 0;
+        
+//          while(current != null){
+//             current = current.next;
+//             length++;
+//          }
+
+//          int middleNodeIndex = length / 2;
+
+//         current = head;
+//          for(int i = 0; i < middleNodeIndex-1; i++){
+//             current = current.next;
+//          }
+
+//           current.next  = current.next.next;
+
+//           return head;
+//     }
+// }
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode current = head;
-        if(head.next == null) return head;
-        int length = 0;
+        if (head == null || head.next == null) return null;
         
-         while(current != null){
-            current = current.next;
-            length++;
-         }
-
-         int middleNodeIndex = length / 2;
-
-        current = head;
-         for(int i = 0; i < middleNodeIndex-1; i++){
-            current = current.next;
-         }
-
-          current.next  = current.next.next;
-
-          return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next; // 두칸씩 while문이 끝나면 fast는 마지막노드
+            prev = slow; //삭제할 전 노드가 된다
+            slow = slow.next;//한칸씩 while문이 끝나면 slow는 삭제할 노드가 됨
+        }
+        
+        prev.next = slow.next;
+        
+        return head;
     }
 }
